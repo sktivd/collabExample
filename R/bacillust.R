c93f15e3b6965d5cc5fa3216b2b1b91a16415703
bacillust <bacillust@gmail.com>
2017-08-25 16:41:50 +0900

    modified bacillust.R file

--- R/bacillust.R
+++ R/bacillust.R
@@ -1,11 +1,9 @@
 #' Say Hello
 #'
-#' @param whom name to say hello
-#' @return the say hello string to \code{whom}
 #' Say Hello with repeatition 
 #'
 #' @param iter num of say hello
-#' @returns the say string with iter nub
+#' @return the say string with iter nub
 #' @examples 
 #' sayHelloRepeat(3)
 #' @export
